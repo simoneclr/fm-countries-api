6e44ce0689210cfe233c17191268415214d04ec0
simoneclr <simone.clclr@gmail.com>
2022-03-02 15:58:49 +0100

    Add dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   HashRouter as Router,
   Routes,
@@ -8,11 +8,37 @@ import {
 import CountriesDashboard from "./components/CountriesDashboard";
 import CountryPage from "./components/CountryPage";
 
+const THEMES = {
+  light: "light-theme",
+  dark: "dark-theme"
+}
+
 function App() {
+
+  // State variable to controle the theme switcher
+  const [theme, setTheme] = useState(THEMES.light)
+
+  const handleThemeChange = (e) => {
+    // If the toggle is ckecked, enable dark theme; otherwise, enable light
+    let updatedTheme = e.target.checked ? THEMES.dark : THEMES.light
+
+    setTheme(updatedTheme)
+  }
+
   return (
-    <div className="app light-theme">
+    <div className={"app " + theme}>
       <header className="app-header">
         <h1 className="app-title">Where in the World?</h1>
+
+        <label className="theme-toggle">
+          <span className="toggle-label">Dark Mode</span>
+
+          <span className="toggle-control">
+            <input type="checkbox" name="theme" id="theme-toggle"
+              checked={theme === THEMES.dark} onChange={handleThemeChange}/>
+            <span className="toggle-slider"></span>
+          </span>          
+        </label>
       </header>
 
       <Router>
