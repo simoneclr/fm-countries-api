0e7fda2eaf1f3eaef42ca3dbb95df5a4b08de2eb
simoneclr <simone.clclr@gmail.com>
2022-10-12 16:12:21 +0200

    Add error handling to CountryNeighborsList

--- src/components/CountryNeighborsList.js
+++ src/components/CountryNeighborsList.js
@@ -3,31 +3,61 @@ import { Link } from "react-router-dom";
 
 import countriesService from "../services/countriesService";
 
+const STATUS = {
+	ok: "OK",
+	loading: "LOADING",
+	error: "ERROR"
+}
+
 function CountryNeighborsList(props) {
+
+	const [status, setStatus] = useState(STATUS.ok)
 
 	// State variable containing the list of neighbors names to display
 	const [neighbors, setNeighbors] = useState([])
 
 	useEffect(() => {
 		setNeighbors([])
+		setStatus(STATUS.loading)
 
 		props.list.forEach(n => {
 			countriesService.getCountryById(n)
 				.then(data => {
 					setNeighbors(prevState => [...prevState, {id: data.id, name: data.name}])
 				})
+				.catch(error => {
+					console.log("An error has occurred ", error.message)
+					setStatus(STATUS.error)
+				})
 		})
+
+		setStatus(prevState => prevState === STATUS.error ? STATUS.error : STATUS.ok)
 	}, [props.list])
 
 	return (
 		<div className="country-neighbors">
 			<span>Border Countries:</span>
 
-			<ul>
-				{neighbors.map(n => <li key={n.id}>
-						<Link to={"/country/" + n.id} className="btn-link">{n.name}</Link>
-					</li>)}
-			</ul>
+			{ (status === STATUS.loading) && 
+				<span className="status-message">
+					Loading neighboring Countries Info...
+				</span>
+			}
+
+			{ (status === STATUS.error) && 
+				<span className="status-message">
+					An error occurred while loading neighboring Countries Info.
+				</span>
+			}
+
+			{ (status === STATUS.ok) &&
+				<ul>
+					{neighbors.map(n => <li key={n.id}>
+							<Link to={"/country/" + n.id} className="btn-link">{n.name}</Link>
+						</li>)}
+				</ul>
+			}
+			
 		</div>
 	)
 }
