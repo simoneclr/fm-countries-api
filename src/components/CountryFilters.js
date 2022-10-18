651091d651464f7a10c5a8fbef57047e63e8b049
simoneclr <simone.clclr@gmail.com>
2022-10-18 15:54:58 +0200

    Add cancel button to search box

--- src/components/CountryFilters.js
+++ src/components/CountryFilters.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { ReactComponent as IconSearch } from "../assets/svgs/magnifying-glass-solid.svg"
+import { ReactComponent as IconClose } from "../assets/svgs/xmark-solid.svg"
+
 function CountryFilters({search, updateSearch, region, updateRegion}) {
 
 	// Function that handles the change of the controls
@@ -13,21 +16,24 @@ function CountryFilters({search, updateSearch, region, updateRegion}) {
 		}
 	}
 
+	const handleCancelClick = (e) => {
+		updateSearch("")
+	}
+
 	return (
 		<div className="filters-menu">
-			<label className="filter-element search-bar">
+			<label className="filter-element search-bar">				
 				<input type="text" name="name-search" value={search} onChange={handleChange}
 							placeholder="Search for a Country..."/>
 
-				<button type="submit" className="btn-icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-						{/*
-						Font Awesome Free 5.15.0 by @fontawesome - https://fontawesome.com
-						License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
-						*/}
-						<path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/>
-					</svg>
-				</button>
+				<IconSearch className="icon icon-search" />
+
+				{ search.length > 0 &&
+					<button className="btn-icon" onClick={handleCancelClick}>
+						<IconClose className="icon icon-cancel" />
+					</button>
+				}
+				
 			</label>
 
 			<select name="region" value={region} onChange={handleChange} 
